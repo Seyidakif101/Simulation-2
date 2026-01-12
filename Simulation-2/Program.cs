16e0abd677d3523c057cdfcca320e5369bd907c8
Seyidakif101 <sayidakhifia-mpa101@code.edu.az>
2026-01-13 00:07:52 +0400

    Add project files.

---
+++ Simulation-2/Program.cs
@@ -0,0 +1,29 @@
+namespace Simulation_2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+           
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
